a8ab02056555980119a26da8bc7ce2f23a758ea2
rohan-rokade <officialyrohanrokade@gmail.com>
2020-07-07 22:35:23 +0530

    sec

--- sihh.py
+++ sihh.py
@@ -4,7 +4,7 @@ import numpy as np
 from random import choice as c
 import matplotlib.pyplot as plt
 
-dataset=pd.read_csv("https://github.com/Rohan-Rokade/networkdetector/blob/master/sihdata.csv",error_bad_lines=False)
+dataset=pd.read_csv("https://github.com/Rohan-Rokade/networkdetector/blob/master/sihdata.csv",sep=',')
 
 st.title('NETWORK DETECTION')
 st.write(dataset)
