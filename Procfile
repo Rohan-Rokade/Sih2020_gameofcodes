e8a003d381dc45c84154eadc31e30c70670570be
rohan-rokade <officialyrohanrokade@gmail.com>
2020-07-08 13:50:37 +0530

    sec

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run sihh.py
+web: sh setup.sh && streamlit run login.py
---
+++ login.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import sqlite3
+
+
+conn=sqlite3.connect('data.db')
+c=conn.cursor()
+
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS usertable(username TEXT,password TEXT)')
+
+def add_userdata(username,password):
+    c.execute('INSERT INTO usertable(username,password) VALUES(?,?)',(username,password))
+    conn.commit()
+
+def login_user(username,password):
+    c.execute('SELECT * FROM usertable WHERE username=? AND password=? ',(username,password))
+    data=c.fetchall()
+    return data
+
+def view_all_users():
+    c.execute('SELECT * FROM usertable')
+    data=c.fetchall()
+    return data
+
+def main():
+    menu=["Home ","login","sign up"]
+    choice=st.sidebar.selectbox("Menu",menu,index=0)
+
+    if choice =="Home":
+        st.subheader("Home")
+
+    elif choice=="login":
+        st.subheader("login section")
+
+        username=st.sidebar.text_input("User Name")
+        password=st.sidebar.text_input("Password",type="password")
+        
+        if st.sidebar.checkbox("Login"):
+            
+            create_usertable()
+            result=login_user(username,password)
+
+            if result:
+                
+                st.success("Logged in as {}".format(username))
+
+                task=["Analytics","Profiles","engineer"]
+                st.selectbox("Task",task)
+
+                if task == "engineer":
+                    st.subheader("Engineer")
+
+                elif task=="Analytics":
+                    st.subheader("Analytics")
+
+                elif task == "Profiles":
+                    st.subheader("Profiles")
+            else :
+                st.warning("Incorrect username/Password")
+            
+
+
+    elif choice == "sign up":
+        st.subheader("Create New Account")
+
+        new_user=st.text_input("Username")
+        new_password=st.text_input("Password",type="password")
+
+        if st.button("Sign Up"):
+            create_usertable()
+            add_userdata(new_user,new_password)
+            st.success("You have successfully created a account")
+            st.info("go to login")
+        
+       
+
+        
+
+
+if __name__== '__main__':
+    main()
+        
